6fe60961b054fc129e6c608a777dc2ad86afcc5f
Calvin Smoot <calvin@xeosmoot.com>
2022-08-17 16:35:21 -0700

    Providing strong typing for the window

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -1,12 +1,19 @@
+import { Router } from './router';
+
 export interface FlamethrowerOptions {
-    log?: boolean;
-    prefetch?: boolean,
-    pageTransitions?: boolean,
-  }
-  
-  export interface RouteChangeData {
-    type: 'link' | 'popstate' | 'noop' | 'disqualified' | 'scroll' | 'go' | string;
-    next?: string;
-    prev?: string;
-  }
-  
+  log?: boolean;
+  prefetch?: boolean;
+  pageTransitions?: boolean;
+}
+
+export interface RouteChangeData {
+  type: 'link' | 'popstate' | 'noop' | 'disqualified' | 'scroll' | 'go' | string;
+  next?: string;
+  prev?: string;
+}
+
+export type FlameRouter = {
+  flamethrower?: Router;
+};
+
+export type FlameWindow = Window & typeof globalThis & FlameRouter;
