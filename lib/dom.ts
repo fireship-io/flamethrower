c161f6cd0499b8ddfe6cdf93922fd7ca37d47e16
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-08-17 15:02:53 +0400

    :zap: Early Optimizations

--- lib/dom.ts
+++ lib/dom.ts
@@ -2,50 +2,35 @@
  * @param  {string} html
  * Convert an HTML string to new Document
  */
-export function formatNextDocument(html: string) {
-  var parser = new DOMParser();
-  const nextDoc = parser.parseFromString(html, 'text/html');
-  return nextDoc;
+export function formatNextDocument(html: string): Document {
+  const parser = new DOMParser();
+  return parser.parseFromString(html, 'text/html');
 }
 
 /**
  * @param  {Document} nextDoc
  * Replace Body
  */
-export function replaceBody(nextDoc: Document) {
-  // document.body.replaceWith(nextDoc.body);
-  document.body.innerHTML = nextDoc.body.innerHTML;
+export function replaceBody(nextDoc: Document): void {
+  document.body.replaceWith(nextDoc.body);
 }
 
 /**
  * @param  {Document} nextDoc
  * Merge new head data
  */
-export function mergeHead(nextDoc: Document) {
-  const currentHead = document.head;
-
+export function mergeHead(nextDoc: Document): void {
   // Update head
   // Head elements that changed on next document
   // TODO make this algo more efficient
-  const old = Array.from(document.head.children);
+  const old = Array.from(document.querySelectorAll('head>:not([rel="prefetch"]'));
   const next = Array.from(nextDoc.head.children);
-  const freshNodes = next.filter(
-    (newNode) => !old.find((oldNode) => oldNode.isEqualNode(newNode))
-  );
-  const staleNodes = old.filter(
-    (oldNode) => !next.find((newNode) => newNode.isEqualNode(oldNode))
-  );
+  const freshNodes = next.filter((newNode) => !old.find((oldNode) => oldNode.isEqualNode(newNode)));
+  const staleNodes = old.filter((oldNode) => !next.find((newNode) => newNode.isEqualNode(oldNode)));
 
-  staleNodes.forEach((node) => {
-    if (node.getAttribute('rel') === 'prefetch') {
-      return;
-    }
-    node.remove();
-  });
+  staleNodes.forEach((node) => node.remove());
 
-  freshNodes.forEach((node) => {
-    currentHead.appendChild(node);
-  });
+  document.head.append(...freshNodes);
 }
 
 /**
@@ -53,25 +38,23 @@ export function mergeHead(nextDoc: Document) {
  * head scripts will only run with data-reload attr
  * all body scripts will run
  */
-export function runScripts() {
+export function runScripts(): void {
   // Run scripts with data-reload attr
-  const headScripts = Array.from(
-    document.head.querySelectorAll('[data-reload]')
-  );
+  const headScripts = document.head.querySelectorAll('[data-reload]');
   headScripts.forEach(replaceAndRunScript);
 
   // Run scripts in body
-  const bodyScripts = Array.from(document.body.querySelectorAll('script'));
+  const bodyScripts = document.body.querySelectorAll('script');
   bodyScripts.forEach(replaceAndRunScript);
 }
 
 // Private helper to re-execute scripts
-async function replaceAndRunScript(oldScript: HTMLScriptElement) {
+function replaceAndRunScript(oldScript: HTMLScriptElement): void {
   const newScript = document.createElement('script');
   const attrs = Array.from(oldScript.attributes);
   for (const { name, value } of attrs) {
-    newScript.setAttribute(name, value);
+    newScript[name] = value;
   }
-  newScript.appendChild(document.createTextNode(oldScript.innerHTML));
-  oldScript.parentNode.replaceChild(newScript, oldScript);
+  newScript.append(oldScript.textContent);
+  oldScript.replaceWith(newScript);
 }
