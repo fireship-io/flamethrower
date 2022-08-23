11dc74972aefbf2cc7d8c1a58c33aa8a37e4bce3
vinay kulkarni <94120295+vinaykulk621@users.noreply.github.com>
2022-08-24 02:19:30 +0530

    🔠typo

--- lib/handlers.ts
+++ lib/handlers.ts
@@ -29,7 +29,7 @@ export function addToPushState(url: string) {
   }
 }
 
-// Smooth stroll to anchor link
+// Smooth scroll to anchor link
 export function scrollToAnchor(anchor) {
   document
     .querySelector(anchor)
